2aea23cecf337ea4dd444f5c31326e85e369edaf
unknown <brianna.albo@gmail.com>
2023-03-19 16:50:11 -0400

    added a photo of myself and written content to the about me section, began styling

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import me from './projectpics/meportfolio.jpg';
+import '../App.css'
 
 const styles = {
     h1: {
         color: 'orange',
+    },
+    aboutme: {
+        width: '100%',
+        justifyContent: 'center',
+        float: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    img: {
+        float: 'right',
+        width: '300px',
+        height: '400px',
+        paddingRight: '20px',
+        borderRadius: '25%'
+    },
+    text: {
+        width: '50%',
+        paddingLeft: '20px',
+        fontSize: '24px'
     }
-};
+}
 
 function About() {
     return (
-       <h1 style={styles.header}>INSERT ABOUT ME HERE</h1>
+        <div id='aboutme'>
+     
+
+       <div class='card' style={styles.text}>
+       <img style= {styles.img}
+            src={me}
+          ></img>
+        <p>I have always been passionate about learning. Throughout my time in various colleges I
+             would joke that "I wish I could make a career out of being a student." That is exactly 
+             what I found in the field of Web Development. I deeply enjoy being in an everchanging field 
+             where learning is a constant. I began my coding journey in the summer of 2022 by watching tutorials 
+             and doing some research on my own. By the Fall of 2022, I enrolled in the Rutgers Coding Bootcamp.
+             In addition to developing skills in Web Development, the Rutgers program allowed me the opportunity to
+             collaborate with other developers to build projects. </p>
+       
+       
+          </div>
+          </div>
     );
 };
 

f4acfb89c2671146bb7f5bce2dbef2a45a36d0a5
unknown <brianna.albo@gmail.com>
2023-03-19 14:53:03 -0400

    playing with styling, added flex wrap for project cards

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import reflection from './projectpics/reflection.png';
+import '../App.css'
 
 const styles = {
     h1: {
         color: 'orange',
     },
     projects: {
+        width: '100%',
         justifyContent: 'center',
         float: 'center',
-        textAlign: 'center'
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    projectsCard: {
+        
+        
+        textAlign: 'center',
+        width: '375px',
+        height: '375px',
+        
     },
     img: {
         width: '350px',
@@ -18,16 +29,31 @@ const styles = {
 
 function Projects() {
     return (
-        <div style={styles.projects}>
+        <div id='portfolio'>
        
-       <div class='card'>
+       <div class='card' style={styles.projectsCard}>
         <h3>Mood Tracker</h3>
         <a href="https://daily-mood.herokuapp.com/" target="_blank"><img style= {styles.img}
             src={reflection}
           ></img></a>
       
-        <article></article>
+    
 </div>
+<div class='card' style={styles.projectsCard}>
+        <h3>Mood Tracker</h3>
+        <a href="https://daily-mood.herokuapp.com/" target="_blank"><img style= {styles.img}
+            src={reflection}
+          ></img></a>
+      
+        
+</div>
+<div class='card' style={styles.projectsCard}>
+        <h3>Mood Tracker</h3>
+        <a href="https://daily-mood.herokuapp.com/" target="_blank"><img style= {styles.img}
+            src={reflection}
+          ></img></a>
+      
+        </div>
        </div>
     );
 };

cfd43a28299610185a4e873a87d7838a3415375f
unknown <brianna.albo@gmail.com>
2023-03-19 14:38:59 -0400

    create projectpics folder with images, began linking my existing projects to the project page

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import reflection from './projectpics/reflection.png';
 
 const styles = {
     h1: {
         color: 'orange',
+    },
+    projects: {
+        justifyContent: 'center',
+        float: 'center',
+        textAlign: 'center'
+    },
+    img: {
+        width: '350px',
+        height: '300px'
     }
 };
 
 function Projects() {
     return (
-       <h1 style={styles.header}>All My Projects here</h1>
+        <div style={styles.projects}>
+       
+       <div class='card'>
+        <h3>Mood Tracker</h3>
+        <a href="https://daily-mood.herokuapp.com/" target="_blank"><img style= {styles.img}
+            src={reflection}
+          ></img></a>
+      
+        <article></article>
+</div>
+       </div>
     );
 };
 
